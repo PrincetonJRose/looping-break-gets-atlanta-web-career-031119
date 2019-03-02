df5355dc013ecdb0c1725232a0ca8b0802cbf79a
Princeton Rose <decoyzpr@gmail.com>
2019-03-02 05:51:28 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,13 @@
 
 def levitation_quiz
 	#your code here
+  while true
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+    if answer == "Wingardium Leviosa"
+      break
+    end
+  end
 end
 
 
